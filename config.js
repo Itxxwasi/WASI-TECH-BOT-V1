8de1cc9a1efefc5cbaa6308ea30ac91938c8bc33
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-03-21 23:50:47 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
 global.port=5000
 global.email = 'sam@secktor.live'
-global.github = 'https://github.com/SamPandey001/Secktor-Md'
-global.location = 'Sultanpur IN'
+global.github = 'https://github.com/SuhailTechInfo/Secktor-Md'
+global.location = 'Lahore PK'
 global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '919628516236'
-global.devs = '919628516236';
-global.website = 'https://github.com/SamPandey001/Secktor-Md' //wa.me/+91000000000000
+global.sudo = process.env.SUDO || '923184474176'
+global.devs = '923184474176';
+global.website = 'https://github.com/SuhailTechInfo/Secktor-Md' //wa.me/+91000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
 module.exports = {
   botname: process.env.BOT_NAME || '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨',
-  ownername:process.env.OWNER_NAME || 'SamPandey001',
+  ownername:process.env.OWNER_NAME || 'SuhailTechInfo',
   sessionName: process.env.SESSION_ID || 'PUT-HERE',
   author: process.env.PACK_INFO.split(";")[0] || 'author', 
   auto_read_status : process.env.AUTO_READ_STATUS || 'false',
@@ -24,17 +24,17 @@ module.exports = {
   autoreaction: process.env.AUTO_REACTION || 'off',
   antibadword : process.env.ANTI_BAD_WORD || 'nobadwordokey',
   alwaysonline: process.env.ALWAYS_ONLINE || 'false',
-  antifake : process.env.FAKE_COUNTRY_CODE || '',
+  antifake : process.env.FAKE_COUNTRY_CODE || '212',
   readmessage: process.env.READ_MESSAGE || false,
-  HANDLERS: process.env.PREFIX || ['.'],
-  warncount : process.env.WARN_COUNT || 3,
-  disablepm: process.env.DISABLE_PM || "flase",
-  levelupmessage: process.env.LEVEL_UP_MESSAGE || 'false',
+  HANDLERS: process.env.PREFIX || [','],
+  warncount : process.env.WARN_COUNT || 1,
+  disablepm: process.env.DISABLE_PM || "false",
+  levelupmessage: process.env.LEVEL_UP_MESSAGE || 'true',
   antilink: process.env.ANTILINK_VALUES || 'chat.whatsapp.com',
   antilinkaction: process.env.ANTILINK_ACTION || 'remove',
   BRANCH: 'main',
-  ALIVE_MESSAGE: process.env.ALIVE_MESSAGE || '',
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY || 'put-key-here',
+  ALIVE_MESSAGE: process.env.ALIVE_MESSAGE || 'hi ... I M Online...',
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || 'null',
   VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
   LANG: process.env.THEME|| 'SECKTOR',
   WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
