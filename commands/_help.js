50527cbf26fa380bb16f3a171a9996b295924e3e
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-03-22 18:24:11 +0500

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -3,9 +3,9 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
+ * @project_name : Secktor-Md By Suhail Tech
+ * @author : SuhailTech <https://www.youtube.com/c/SuhailTechInfo>
+ * @description : Secktor Bot ,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
 
@@ -105,7 +105,7 @@ Secktor.cmd({
                 let buttonMessaged = {
                     image: { url: await botpic() },
                     caption: str,
-                    footer: tlang().title,
+                    footer: tlang().footer,
                     headerType: 4,
                     buttons: generatebutton
                 };
