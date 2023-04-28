165ca6680eea51d98f3d276be9a5dce2933b8a35
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-04-28 14:20:47 +0500

    Update herokuVars.js

--- commands/herokuVars.js
+++ commands/herokuVars.js
@@ -2,26 +2,23 @@ const Config = require('../config')
 let { fancytext, tlang, tiny, runtime, formatp, botpic, prefix, sck1,cmd } = require("../lib");
 const axios = require('axios');
 const appName = Config.HEROKU_APP_NAME;
-const apiKey = Config.HEROKU_API_KEY;
+const authToken = Config.HEROKU_API_KEY;
 const fetch = require('node-fetch');
 
 
 cmd({
-        pattern: "allvar",
+        pattern: "getallvar",
         desc: "To restart bot",
         category: "tools",
         filename: __filename
     },
     
 async(Void, citel , text) => {
-
-
-const authToken = Config.HEROKU_API_KEY; // Replace with your Heroku auth token
+        
 const headers = {
   'Accept': 'application/vnd.heroku+json; version=3',
   'Authorization': `Bearer ${authToken}`
 };
-const appName =Config.HEROKU_APP_NAME;
 
 
 fetch(`https://api.heroku.com/apps/${appName}/config-vars`, { headers })
@@ -44,3 +41,40 @@ fetch(`https://api.heroku.com/apps/${appName}/config-vars`, { headers })
   return citel.reply('Error retrieving app variables:\n');
   
 });
+//----------------------------------------------------------------------------------
+cmd({
+        pattern: "setvar",
+        desc: "To restart bot",
+        category: "tools",
+        filename: __filename
+    },
+    
+async(Void, citel , text) => {
+
+if (!text) return citel.reply (`give me Variable Name\n*Example : ${prefix}setvar AUTO_READ_STATUS:true *`);
+
+const headers = 
+        {
+                'Accept': 'application/vnd.heroku+json; version=3',
+                 'Authorization': `Bearer ${authToken}`,
+                 'Content-Type': 'application/json'
+        };
+
+const varName = text.split(":")[0];
+const newVarValue = text.split(":")[1]; 
+
+fetch(`https://api.heroku.com/apps/${appName}/config-vars`,
+        {
+         method: 'PATCH',
+          headers,
+         body: JSON.stringify({ [varName]: newVarValue })
+        })
+  .then(response => response.json())
+  .then(data => 
+          {
+                console.log(data);
+                return citel.reply(`Variable *${varName}* has been updated.`);
+           })
+  
+  .catch(error => console.error('Error updating app variable:', error));
+  });
