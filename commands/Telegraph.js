2a0390b20f987e9a90d19f762839e22801255ed2
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-04-29 18:03:04 +0500

    Update Telegraph.js

--- commands/Telegraph.js
+++ commands/Telegraph.js
@@ -13,6 +13,22 @@
 const { cmd }   = require('../lib');
 const util = require('util');
 const axios = require('axios');
+const PastebinAPI = require("pastebin-js");
+pastebin = new PastebinAPI("EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL");
+cmd({
+        pattern: "pastebin",
+        desc: "To check ping",
+        category: "converter",
+        filename: __filename,
+    },
+    async(Void, citel,text) => {
+ if (!text) { text=citel.quoted.text;}
+        if(!text) return citel.reply('Please reply to any text to get link.');
+        let data = await pastebin.createPaste(text, "Suhail Tech Info");
+        return citel.reply('_Here is your link._\n'+data+'\n*Click to Get Your Text*');
+    }
+);
+//--------------------------------------------------------------------------
 cmd({
         pattern: "paste",
         desc: "create paste of text.",
