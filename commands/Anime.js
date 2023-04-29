db0302ddfe1473c7243d1df7882d45ea023ee996
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-04-29 11:37:49 +0500

    Update Anime.js

--- commands/Anime.js
+++ commands/Anime.js
@@ -1,23 +1,33 @@
+const Config = require('../config')
+let { fancytext, tlang, tiny, runtime, formatp, botpic, prefix, sck1,cmd } = require("../lib");
+const axios = require('axios');
+const appName = Config.HEROKU_APP_NAME;
+const authToken = Config.HEROKU_API_KEY;
+const fetch = require('node-fetch');
 
 
+cmd({
+        pattern: "waifu",
+        desc: "To get Waifu Random Pics",
+        category: "Anime Chars",
+        filename: __filename
+    },
 
-
-
-
-
-
-
+    async(Void, citel, text) => {
 
 
   const response = await fetch('https://api.waifu.pics/sfw/neko');
   const data = await response.json();
-  citel.reply ('url here :'+data.url);
-
+  
                 let buttonMessaged = {
                     image: { url: data.url },
-                    caption: 'Amime Pics Here',
+                    caption: '*---Waifu Pics Here---*',
                     footer: tlang().footer,
                     headerType: 4,
                    
                 };
                 return await Void.sendMessage(citel.chat, buttonMessaged);
+}
+    )
+
+//-----------------------------------------------------------------------
