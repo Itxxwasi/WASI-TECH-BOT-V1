538af095e9cf11b8f905c34eb79d450a74180fc2
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-04-29 13:24:05 +0500

    Update Anime.js

--- commands/Anime.js
+++ commands/Anime.js
@@ -34,6 +34,43 @@ cmd({
     )
 //-----------------------------------------------------------------------
 cmd({
+    pattern: "neko",
+    category: "Anime Chars",
+    desc: "Sends a Neko Image in chat",
+    filename: __filename
+},
+async(Void, citel, text) => {
+    nekodds = await axios.get("https://waifu.pics/api/sfw/neko");
+    var wbuttsssrd = [{
+            buttonId: `${prefix}neko`,
+            buttonText: {
+                displayText: `Neko`,
+            },
+            type: 1,
+        },
+
+        {
+            buttonId: `${prefix}waifu`,
+            buttonText: {
+                displayText: `Waifu`,
+            },
+            type: 1,
+        },
+    ];
+    let button4Messagesp = {
+        image: {
+            url: nekodds.data.url,
+        },
+        caption: "*Here we go😊!!!!*",
+        headerType: 1,
+    };
+    await Void.sendMessage(citel.chat, button4Messagesp, {
+        quoted: citel,
+    })
+}
+)
+//-----------------------------------------------------------------------
+cmd({
         pattern: "animenews",
         category: "Anime Chars",
         desc: "Anime News",
