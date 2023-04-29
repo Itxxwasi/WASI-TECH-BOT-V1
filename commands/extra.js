0571c8274fe2411f8f737f4bda72d5f7394961ec
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-04-29 18:03:10 +0500

    Update extra.js

--- commands/extra.js
+++ commands/extra.js
@@ -11,19 +11,3 @@
  *
  */
 
-const {cmd} = require('../lib')
-const PastebinAPI = require("pastebin-js");
-pastebin = new PastebinAPI("EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL");
-cmd({
-        pattern: "pastebin",
-        desc: "To check ping",
-        category: "converter",
-        filename: __filename,
-    },
-    async(Void, citel,text) => {
- if (!text) { text=citel.quoted.text;}
-        if(!text) return citel.reply('Please reply to any text to get link.');
-        let data = await pastebin.createPaste(text, "Suhail Tech Info");
-        return citel.reply('_Here is your link._\n'+data+'\n*Click to Get Your Text*');
-    }
-);
