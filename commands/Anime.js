f54879543d95d77d527884bdcaa5930882228b0b
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-04-29 13:21:06 +0500

    Update Anime.js

--- commands/Anime.js
+++ commands/Anime.js
@@ -4,6 +4,9 @@ const axios = require('axios');
 const appName = Config.HEROKU_APP_NAME;
 const authToken = Config.HEROKU_API_KEY;
 const fetch = require('node-fetch');
+const axios = require('axios')
+const { Anime, Manga } = require("@shineiichijo/marika");
+const {  fetchJson, getBuffer} = require('../lib/')
 
 
 cmd({
@@ -29,7 +32,46 @@ cmd({
                 return await Void.sendMessage(citel.chat, buttonMessaged);
 }
     )
+//-----------------------------------------------------------------------
+cmd({
+        pattern: "animenews",
+        category: "Anime Chars",
+        desc: "Anime News",
+         filename: __filename
+    },
+    async(Void, citel, text) => {
+        let qq = [
+            "Anime News Today",
+            "New Anime",
+            "Uocoming Anime News",
+            "New Anime Info",
+            "Whats news in Anime",
+            "Anime Series",
+            "Manga News today",
+            "Anime New News",
+            "Anime News today",
+        ];
+        let q1 = qq[Math.floor(Math.random() * qq.length)];
+        //&domains=techcrunch.com,animenewsnetwork.com,myanimelist.net,comingsoon.net,crunchyroll.com
+        let URL1 = `https://newsapi.org/v2/everything?q=${q1}&domains=techcrunch.com,animenewsnetwork.com,myanimelist.net,comingsoon.net,crunchyroll.com&language=en&sortby=publishedat&apikey=cd4116be09ef4a0caceedf21b6258460&pageSize=8`;
+        const response = await axios.get(URL1);
+        let result = await response;
+        result = result.data.articles;
+        result.map(async(r, idx) => {
+            Void.sendMessage(
+                citel.chat, {
+                    image: { url: r.urlToImage },
+                    caption: `*Title🔰:* ${r.title}\n\n*Content🧩:* ${r.content}\n*Author📌:* ${r.author}\n*Source♦️:* ${r.source.name}\n*Created On☘️:* ${r.publishedAt}\n*More on✨:* ${r.url}\n\n*Powered by ${tlang().title}*`,
+                }, {
+                    quoted: citel,
+                }
 
+            );
+        });
+
+
+    }
+)
 //-----------------------------------------------------------------------
 
 cmd({
