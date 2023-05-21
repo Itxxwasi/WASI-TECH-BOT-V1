dba6cc0f760669b2b023729dda64f72e501bf453
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-05-21 12:35:44 +0500

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -391,6 +391,30 @@ async(Void, citel, text) => {
 
     //---------------------------------------------------------------------------
 cmd({
+            pattern: "sound",
+            desc: "Downloads ringtone.",
+            category: "downloader",
+            filename: __filename,
+            use: '<Dowanload Tiktok Sounds>',
+        },
+        async(Void, citel, text) => {
+            if (!text) return citel.reply(`*Give A Number Example: ${prefix}sound 5*`)
+	const n = parseInt(text);
+	if(n.toString()=="NaN" || n < 1 || n > 160 ) return citel.reply('```❌ Give Me A Number From 1 to 160```');
+	   let url = `https://github.com/DGXeon/Tiktokmusic-API/raw/master/tiktokmusic/sound${n.toString()}.mp3`
+            let anu  = await getBuffer(url)
+//await Void.sendMessage(citel.chat, { audio: botzy_buffer, mimetype: 'audio/mp4', ptt: true })
+        let buttonMessage = {
+		audio: anu,
+		fileName: url.toString() ,
+		mimetype: 'audio/mp4',
+		ptt: true 
+		}
+	return Void.sendMessage(citel.chat,buttonMessage, { quoted: citel } )
+})
+
+    //---------------------------------------------------------------------------
+cmd({
             pattern: "ringtone",
             desc: "Downloads ringtone.",
             category: "downloader",
