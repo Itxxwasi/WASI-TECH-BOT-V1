7930243d2e0f084b605ca87a6b64f4f0e4c89d43
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-05-26 00:18:03 +0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,15 +1,4 @@
-/**
- Copyright (C) 2022.
- Licensed under the  GPL-3.0 License;
- You may not use this file except in compliance with the License.
- It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
- * @version 0.0.6
- **/
-
-//---------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
 const Secktor = require('../lib')
 Secktor.cmd({
         pattern: "ping",
@@ -19,7 +8,7 @@ Secktor.cmd({
     },
     async(Void, citel) => {
         var inital = new Date().getTime();
-        await citel.reply('```Ping!!!```');
+        await citel.reply('```Testing Ping!!!```');
         var final = new Date().getTime();
         return await citel.reply('*Pong*\n *' + (final - inital) + ' ms* ');
     }
