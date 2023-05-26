4f3f5c2df63c1ab75b3ae33505530a446915dbb5
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-05-26 21:46:16 +0500

    ATTP Added

--- commands/converter.js
+++ commands/converter.js
@@ -106,6 +106,35 @@ else return citel.reply("```This is Not A ViewOnce Message```")
  //---------------------------------------------------------------------------
  //---------------------------------------------------------------------------
 cmd({
+            pattern: "attp",
+            alias: ["circlestic","circlesticker","cs"],
+            desc: "Makes sticker of replied image/video.",
+            category: "sticker",
+filename: __filename,
+            use: '<reply to any image/video.>'
+        },
+        async(Void, citel, text) => {
+if(!text) return citel.reply("```Uhh Please, Give Me text```")
+let url = `https://raganork-api.onrender.com/api/attp?text=${text}&apikey=with_love_souravkl11`
+let media  = await getBuffer(url)
+
+                let sticker = new Sticker(media, {
+                    pack: Config.packname, 
+                    author: Config.author, 
+                    type: StickerTypes.FULL,
+                    categories: ["🤩", "🎉"], 
+                    id: "12345", 
+                    quality: 100,
+                    background: "transparent", 
+                });
+                const buffer = await sticker.toBuffer();
+                return Void.sendMessage(citel.chat, {sticker: buffer}, {quoted: citel });
+
+        }
+    )
+    //---------------------------------------------------------------------------
+ //---------------------------------------------------------------------------
+cmd({
             pattern: "sticker",
             alias: ["s"],
             desc: "Makes sticker of replied image/video.",
