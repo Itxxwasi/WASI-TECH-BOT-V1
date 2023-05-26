0d2171d32dffb76bdf8c9de16555fac1b5f317de
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-05-26 22:27:55 +0500

    Google Images Added

--- commands/search.js
+++ commands/search.js
@@ -199,10 +199,55 @@ cmd({
             filename: __filename,
         },
         async(Void, citel, text) => {
-            if (!text) return citel.reply(`Provide me a query!\n*Ex : .image luffy*`)
-            let buttonMessage = {}
-            let name1 = text.split("|")[0] || `Luffy`
-            let name2 = text.split("|")[1] || `10`
+
+   if (!text) return citel.reply(`Provide me a query!\n*Ex : .image luffy |10*`)
+   let buttonMessage = {}
+   let name1 = text.split("|")[0] || `Luffy`
+   let name2 = text.split("|")[1] || `10`
+ try {
+    let urlsArray = [];
+    const params = {
+        q: name1, 
+        tbm: "isch",
+        hl: "en",
+        gl: "in",
+        ijn: "0", 
+    };
+    const headers = {
+      "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36",
+      "Accept-Encoding": "application/json",
+  };
+  
+    const res = await axios.get("https://www.google.com/search", { headers: headers, params: params });
+    let body = res.data;
+    body = body.slice(body.lastIndexOf("AF_initDataCallback"));
+    body = body.slice(body.indexOf("["));
+    body = body.slice(0, body.indexOf("</script>")-1);
+    body = body.slice(0, body.lastIndexOf(","));
+    
+    const img = JSON.parse(body);
+
+    const imgObjects = img[56][1][0][0][1][0];
+    for (let i = 0; i < name2; i++) {
+        if (imgObjects[i] && imgObjects[i][0][0]["444383007"][1]) {
+            let url = imgObjects[i][0][0]["444383007"][1][3][0]; // the url
+            urlsArray.push(url);
+        }
+    }
+
+for (let url of urlsArray) 
+{
+  Void.sendMessage(citel.chat , {image : {url : url} , caption : Config.caption} , {quoted:citel})
+}
+   return 
+} 
+ catch (error) {   return citel.reply("*Google Images Not Working, Try it Later*"); }
+
+ 
+ 
+ /*
+ 
 let isImages = false;
             let num = text.split("|")[1];
  gis(name1, async(error, result) => { 
@@ -230,6 +275,9 @@ else return citel.reply("*Google Images Not Working, Try it Later*");
              Void.sendMessage(citel.chat, buttonMessage, { quoted: citel });
                 })
             }
+            
+            
+            */
  })
     //---------------------------------------------------------------------------
 cmd({
