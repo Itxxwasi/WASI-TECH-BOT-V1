8d418d7bbb6c5e407f46c37b03c5b46518a3fa4d
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-05-28 23:40:19 +0500

    SET SUDO ADDED

--- commands/herokuVars.js
+++ commands/herokuVars.js
@@ -6,6 +6,93 @@ const authToken = Config.HEROKU_API_KEY;
 const fetch = require('node-fetch');
 
 if(Config.HEROKU_APP_NAME && Config.HEROKU_API_KEY ){
+        
+         cmd({
+             pattern: "setsudo",
+             desc: "Makes wa me of quoted or mentioned user.",
+             category: "tools",
+             filename: __filename
+         },
+  async(Void, citel, text) => {
+if(!citel.quoted) return await citel.reply(`*Please Reply A User*`);
+let user = citel.quoted.sender.split('@')[0]
+if (global.sudo.includes(user)) return citel.reply("Number Already Exist In Sudo");
+    global.sudo += ',' + user ;
+const headers = 
+        {
+                'Accept': 'application/vnd.heroku+json; version=3',
+                 'Authorization': `Bearer ${authToken}`,
+                 'Content-Type': 'application/json'
+        };
+const varName = 'SUDO'
+const newVarValue = global.sudo        
+fetch(`https://api.heroku.com/apps/${appName}/config-vars`,
+        {
+                  method: 'PATCH',
+                  headers,
+                  body: JSON.stringify({ [varName]: newVarValue })
+        })
+.then(response => response.json())
+.then(data => { return citel.reply(`*${user} Added Succesfully.*\nSudo Numbers : ${newVarValue}`); })
+.catch(error => citel.reply('Error While Adding new Sudo :'+ error));
+
+         })
+
+//--------------------------------------------------------------------
+ cmd({
+             pattern: "getsudo",
+             desc: "Makes wa me of quoted or mentioned user.",
+             category: "tools",
+             filename: __filename
+         },
+async(Void, citel, text) => {  return await  citel.reply(global.sudo);})
+//-------------------------------------------------------------------------
+
+ cmd({
+             pattern: "delsudo",
+             desc: "Makes wa me of quoted or mentioned user.",
+             category: "tools",
+             filename: __filename
+         },
+  async(Void, citel, text) => {
+    
+if(!citel.quoted) return citel.reply(`*Please Reply A User*`);
+let user = citel.quoted.sender.split('@')[0] ;
+let  rm = ',' +user 
+if (global.sudo.includes(rm)) global.sudo = global.sudo.replace(rm, '');
+else return await citel.reply("User not found in the Sudo List\n Sudo Numbers : " + global.sudo );
+
+
+
+const headers = 
+        {
+                'Accept': 'application/vnd.heroku+json; version=3',
+                 'Authorization': `Bearer ${authToken}`,
+                 'Content-Type': 'application/json'
+        };
+
+const varName = 'SUDO'
+const newVarValue = global.sudo        
+fetch(`https://api.heroku.com/apps/${appName}/config-vars`,
+        {
+          method: 'PATCH',
+          headers,
+          body: JSON.stringify({ [varName]: newVarValue })
+        })
+.then(response => response.json())
+.then(data => 
+      { 
+   console.log(data);
+   return citel.reply(`*${user} Deleted Succesfully.*\nSudo Numbers : ${newVarValue}`);
+      })
+  
+.catch(error => return citel.reply('Error While Adding new Sudo :'+ error));
+ 
+})     
+    
+ //------------------------------------------------------------------------       
+        
+        
 cmd({
         pattern: "allvar",
         alias:['getallvar','allvars'],
