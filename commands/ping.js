c8b567e3bf47839cc828df0947208dcfac1bff0a
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-05-31 14:38:00 +0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -26,16 +26,3 @@ cmd({
 
 //-----------------------------------------------------------------------------------
 
-Secktor.cmd({
-        pattern: "ping",
-        desc: "To check ping",
-        category: "general",
-        filename: __filename,
-    },
-    async(Void, citel) => {
-        var inital = new Date().getTime();
-        await citel.reply('```Testing Ping!!!```');
-        var final = new Date().getTime();
-        return await citel.reply('*Pong*\n *' + (final - inital) + ' ms* ');
-    }
-);
