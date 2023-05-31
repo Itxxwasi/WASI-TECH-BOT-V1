eafc8c9b4bae495a47d1538d6ff5ae9736fc1626
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-05-31 14:44:26 +0500

    Update TextPro_Editor.js

--- commands/TextPro_Editor.js
+++ commands/TextPro_Editor.js
@@ -9,18 +9,24 @@ const fetch = require('node-fetch');
 
 
 //-----------------------------------------------------------------------------------
+cmd({ pattern: "deepsea", category: "textpro", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
+        if (!text) return citel.reply('*_Example : .steel Suhail Tech_*');
+        let anu = await maker.textpro('https://textpro.me/free-advanced-glow-text-effect-873.html', text);
+        Void.sendMessage(citel.chat, { image: { url: anu }, caption: `Made by ${tlang().title},For my ${tlang().greet}` }, { quoted: citel });
+    })
+//---------------------------------------------------------------------------
 cmd({
             pattern: "steel",
             category: "edit",
             desc: "Some text to image feature with various styles."
         },
         async(Void, citel, text) => {
-            if (!text) return citel.reply('_Example : .steel suhail;tech info_')  
-            let text1 = text ? text.split(';')[0] : ''
-            let text2 = text ? text.split(';')[1] : ''
-            if(!text2 || !text1) return await citel.reply("*Uhh Please Provide text. Example: .steel text1;text2*")
-            let anu = await maker.textpro('https://textpro.me/3d-steel-text-effect-877.html', [text1, text2] )
-            Void.sendMessage(citel.chat, { image: {url: anu },caption: `Made by ${tlang().title},For my ${tlang().greet}`}, { quoted: citel })
+            if (!text) return citel.reply('_Example : .steel suhail;tech info_');  
+            let text1 = text ? text.split(';')[0] : '';
+            let text2 = text ? text.split(';')[1] : '';
+            if(!text2 || !text1) return await citel.reply("*Uhh Please Provide text. Example: .steel text1;text2*");
+            let anu = await maker.textpro('https://textpro.me/3d-steel-text-effect-877.html', [text1, text2] );
+            Void.sendMessage(citel.chat, { image: {url: anu },caption: `Made by ${tlang().title},For my ${tlang().greet}`}, { quoted: citel });
         }
     )
 
