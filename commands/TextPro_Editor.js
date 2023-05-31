0cef52ded4078a470e2bf635590b3da47d10d232
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-05-31 15:05:00 +0500

    Update TextPro_Editor.js

--- commands/TextPro_Editor.js
+++ commands/TextPro_Editor.js
@@ -9,7 +9,7 @@ const fetch = require('node-fetch');
 
 
 //-----------------------------------------------------------------------------------
-cmd({ pattern: "glow", category: "textpro", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
+cmd({ pattern: "glow", category: "logo", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
         if (!text) return citel.reply('*_Example : .steel Suhail Tech_*');
         let anu = await maker.textpro('https://textpro.me/free-advanced-glow-text-effect-873.html', text);
         Void.sendMessage(citel.chat, { image: { url: anu }, caption: `Made by ${tlang().title},For my ${tlang().greet}` }, { quoted: citel });
@@ -17,7 +17,7 @@ cmd({ pattern: "glow", category: "textpro", desc: "Some text to image feature wi
 //---------------------------------------------------------------------------
 cmd({
             pattern: "steal",
-            category: "textpro",
+            category: "logo",
             desc: "Some text to image feature with various styles."
         },
         async(Void, citel, text) => {
@@ -31,4 +31,48 @@ cmd({
     )
 
 //-----------------------------------------------------------------------------------
-
+cmd({
+            pattern: "avenger",
+            category: "logo",
+            desc: "Some text to image feature with various styles."
+        },
+        async(Void, citel, text) => {
+            if (!text) return citel.reply('*_Example : .avenger suhail;tech_*');  
+            let text1 = text ? text.split(';')[0] : '';
+            let text2 = text ? text.split(';')[1] : '';
+            if(!text2 || !text1) return await citel.reply("*Uhh Please Provide text. Example: .avenger text1;text2*");
+            let anu = await maker.textpro('https://textpro.me/create-3d-avengers-logo-online-974.html', [text1, text2] );
+            Void.sendMessage(citel.chat, { image: {url: anu },caption: `Made by ${tlang().title},For my ${tlang().greet}`}, { quoted: citel });
+        }
+    )
+//-----------------------------------------------------------------------------------
+cmd({
+            pattern: "marvel",
+            category: "logo",
+            desc: "Some text to image feature with various styles."
+        },
+        async(Void, citel, text) => {
+            if (!text) return citel.reply('*_Example : .marvel suhail;tech_*');  
+            let text1 = text ? text.split(';')[0] : '';
+            let text2 = text ? text.split(';')[1] : '';
+            if(!text2 || !text1) return await citel.reply("*Uhh Please Provide text. Example: .marvel text1;text2*");
+            let anu = await maker.textpro('https://textpro.me/create-logo-style-marvel-studios-ver-metal-972.html', [text1, text2] );
+            Void.sendMessage(citel.chat, { image: {url: anu },caption: `Made by ${tlang().title},For my ${tlang().greet}`}, { quoted: citel });
+        }
+    )
+//-----------------------------------------------------------------------------------
+cmd({
+            pattern: "phub",
+            category: "logo",
+            desc: "Some text to image feature with various styles."
+        },
+        async(Void, citel, text) => {
+            if (!text) return citel.reply('*_Example : .phub suhail;tech_*');  
+            let text1 = text ? text.split(';')[0] : '';
+            let text2 = text ? text.split(';')[1] : '';
+            if(!text2 || !text1) return await citel.reply("*Uhh Please Provide text. Example: .phub text1;text2*");
+            let anu = await maker.textpro('https://textpro.me/pornhub-style-logo-online-generator-free-977.html', [text1, text2] );
+            Void.sendMessage(citel.chat, { image: {url: anu },caption: `Made by ${tlang().title},For my ${tlang().greet}`}, { quoted: citel });
+        }
+    )
+//-----------------------------------------------------------------------------------
