e6ddf75a91606cf524e3004e4bbd106bac8b591f
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-06-03 14:57:40 +0500

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -15,8 +15,9 @@ const {GDriveDl} = require('../lib/scraper.js')
 const fbInfoVideo = require('fb-info-video'); 
 const googleTTS = require("google-tts-api");
 const ytdl = require('ytdl-secktor')
-const axios = require('axios');
-const fs = require('fs-extra')
+const cheerio = require('cheerio')
+const fs  = require('fs-extra');
+const axios= require('axios');
 var videotime = 3600 // 30 min
 var dlsize = 100 // 100mb
     //---------------------------------------------------------------------------
@@ -70,6 +71,92 @@ let vurl=res.url[0].url;
 
 }
 )*/
+
+//---------------------------------------------------------------------------
+
+async function tiktokdl (url) {
+    const gettoken = await axios.get("https://tikdown.org/id");
+    const $ = cheerio.load(gettoken.data);
+    const token = $("#download-form > input[type=hidden]:nth-child(2)").attr("value");
+    const param = {
+        url: url,
+        _token: token,
+    };
+    const { data } = await axios.request("https://tikdown.org/getAjax?", {
+        method: "post",
+        data: new URLSearchParams(Object.entries(param)),
+        headers: {
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36",
+        },
+    });
+    var getdata = cheerio.load(data.html);
+    if (data.status) {
+        return {
+            status: true,
+            thumbnail: getdata("img").attr("src"),
+            video: getdata("div.download-links > div:nth-child(1) > a").attr("href"),
+            audio: getdata("div.download-links > div:nth-child(2) > a").attr("href"),
+        };
+    } else return { status: false };
+};
+
+
+
+
+//---------------------------------------------------------------------------
+
+cmd({
+            pattern: "tiktok",
+	          alias :  ['tt','ttdl'],
+            desc: "Downloads Tiktok Videos Via Url.",
+            category: "downloader",
+            react :'🥳',
+            filename: __filename,
+            use: '<add tiktok url.>'
+        },
+
+        async(Void, citel, text) => {
+if(!text) return await citel.reply(`*Uhh Please, Provide me tiktok Video Url*\n*_Ex .tiktok https://www.tiktok.com/@dakwahmuezza/video/7150544062221749531_*`);
+let txt = text ? text.split(" ")[0]:'';
+ if (!/tiktok/.test(txt)) return await citel.reply(`*Uhh Please, Give me Valid Tiktok Video Url!*`);
+ const { status ,thumbnail, video, audio } = await tiktokdl(txt)
+console.log("url : " , video  ,"\nThumbnail : " , thumbnail ,"\n Audio url : " , audio )
+
+if (status) return await Void.sendMessage(citel.chat, {video : {url : video } , caption : Config.caption } , {quoted : citel });
+else return await citel.reply("Error While Downloading Your Video") 
+
+	
+	
+	
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //---------------------------------------------------------------------------
 /*
