933702254495f19da910cb0accc28058487f005b
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-06-03 14:59:33 +0500

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -108,56 +108,22 @@ async function tiktokdl (url) {
 
 cmd({
             pattern: "tiktok",
-	          alias :  ['tt','ttdl'],
+	    alias :  ['tt','ttdl'],
             desc: "Downloads Tiktok Videos Via Url.",
             category: "downloader",
-            react :'🥳',
             filename: __filename,
             use: '<add tiktok url.>'
         },
 
         async(Void, citel, text) => {
-if(!text) return await citel.reply(`*Uhh Please, Provide me tiktok Video Url*\n*_Ex .tiktok https://www.tiktok.com/@dakwahmuezza/video/7150544062221749531_*`);
-let txt = text ? text.split(" ")[0]:'';
+ if(!text) return await citel.reply(`*Uhh Please, Provide me tiktok Video Url*\n*_Ex .tiktok https://www.tiktok.com/@dakwahmuezza/video/7150544062221749531_*`);
+ let txt = text ? text.split(" ")[0]:'';
  if (!/tiktok/.test(txt)) return await citel.reply(`*Uhh Please, Give me Valid Tiktok Video Url!*`);
  const { status ,thumbnail, video, audio } = await tiktokdl(txt)
-console.log("url : " , video  ,"\nThumbnail : " , thumbnail ,"\n Audio url : " , audio )
-
-if (status) return await Void.sendMessage(citel.chat, {video : {url : video } , caption : Config.caption } , {quoted : citel });
-else return await citel.reply("Error While Downloading Your Video") 
-
-	
-	
-	
+ console.log("url : " , video  ,"\nThumbnail : " , thumbnail ,"\n Audio url : " , audio )
+ if (status) return await Void.sendMessage(citel.chat, {video : {url : video } , caption : Config.caption } , {quoted : citel });
+ else return await citel.reply("Error While Downloading Your Video") 
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //---------------------------------------------------------------------------
 /*
 cmd({
