b18db4e3a432e9e9164c0aa0b4850fab6513fa99
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-06-20 19:28:04 +0500

    Update Anime.js

--- commands/Anime.js
+++ commands/Anime.js
@@ -30,7 +30,29 @@ cmd({
         }
     )
 //-----------------------------------------------------------------------
-
+cmd({
+            pattern: "hug",
+            category: "reaction",
+            use: '<quote|reply|tag>',
+        },
+        async(Void, citel) => {
+            var bite = await fetchJson(`https://api.waifu.pics/sfw/poke`);
+            const response = await axios.get(bite.url, {
+                responseType: "arraybuffer",
+            });
+            const buffer = Buffer.from(response.data, "utf-8");
+            let users = citel.mentionedJid ? citel.mentionedJid[0] : citel.msg.contextInfo.participant || false;
+            let gif = await GIFBufferToVideoBuffer(buffer);
+            if (users) {
+                let cap = `@${citel.sender.split("@")[0]} hug to @${users.split("@")[0]} `;
+                Void.sendMessage(citel.chat, { video: gif, gifPlayback: true, mentions: [users, citel.sender], caption: cap }, { quoted: citel });
+            } else {
+                let cap = `@${citel.sender.split("@")[0]} huged to everyone. `;
+                Void.sendMessage(citel.chat, { video: gif, gifPlayback: true, mentions: [citel.sender], caption: cap }, { quoted: citel });
+            }
+        }
+    )
+//-----------------------------------------------------------------------
 cmd({
         pattern: "waifu",
         desc: "To get Waifu Random Pics",
