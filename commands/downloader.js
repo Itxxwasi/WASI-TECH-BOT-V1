08fda5e20b726bb4414b4c73736f5b9f50722edd
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-07-03 12:08:05 +0500

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -38,33 +38,24 @@ cmd({
 		let res = `Total stickers: ${result.stickers.length}\n*Estimated complete in:* ${result.stickers.length * 1.5} seconds\nKeep in mind that there is a chance of a ban if used frequently`.trim()
 		if (result.is_animated) return await citel.reply("Animated stickers are not supported");
   		else if (check.startsWith("info")) return await citel.reply(res);
-		
 		let limit = parseInt(check.split(",")[0]) || 10;
-		let count =  parseInt(check.split(",")[1]) ||  0   ;
-
+		let count =  parseInt(check.split(",")[1]) ||  0;
+	 	let isCheckText = check.split(";")[1] ||  "Sticker"
+		let isSticker = true ;
+	        if (isCheckText.includes("photo") ){isSticker = false ;	isCheckText = "Photo"}
 		if(limit > result.stickers.length ) {  limit = result.stickers.length  }
-		if(count > limit )
-		{  
-			let temp = limit;
-			limit = count;
-			count = temp ;
-		}
-		await citel.reply(`${res}\n\n_Downloading stickers From index *${count}* to *${limit}*._\nIf you wants more to download then use Like \n\n .tgs ${tgUrl} |  10 ,  20`)
-		for ( count ; count <= limit ; count++) {
+	        if(count > result.stickers.length ) {  count = result.stickers.length - 5  }
+		if(count > limit ){let temp = limit ;   limit = count;	count = temp ;}
+		await citel.reply(`${res}\n\n_Downloading as ${isCheckText} From index *${count}* to *${limit}*._\nIf you wants more to download then use Like \n\n .tgs ${tgUrl} |  10 ,  20 ; photo`)
+		for ( count ; count < limit ; count++) 
+		{
 		 // if (count >= limit) break;
 		  let file_path = await fetchJson(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getFile?file_id=${result.stickers[count].file_id}`);
-		  let a = await getBuffer(`https://api.telegram.org/file/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/${file_path.result.file_path}`);
-		  await citel.reply(a, { packname: Config.packname, author: '𓅋 ₊₉₂⃗⃗₃₁᩺₈ͦ₄ͪ₄ᷧ₇ͥ₄ᷞ₁⃗₇₆ 𓃮\nᴄʟօsᴇ ყσυɾ ᴇყᴇs...👁️🙈\nPlugin Creator\n\nʍɪss ʍᴇ...♥️\n' }, "sticker");
+		  let sticUrl = `https://api.telegram.org/file/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/${file_path.result.file_path}`;
+		  if(isSticker) { let a = await getBuffer(sticUrl); await citel.reply(a, { packname: Config.packname, author: "Suhail-Md"  }, "sticker");} 
+		  else { await Void.sendMessage(citel.chat,{image : {url : sticUrl } , caption : `*_Telegram Sticker At Index ${count+1} Downloaded_*`}) } 
 		  //count++;
 		}
-
-
-
-
-
-
-
-
 
 
  
