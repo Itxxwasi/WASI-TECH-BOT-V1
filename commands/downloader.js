e90644bdd7f6bd5de8f018e38ddbe010b764f940
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2023-07-08 00:03:14 +0500

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -339,7 +339,6 @@ cmd({
 async(Void, citel, text) => {
 if (!text) return citel.reply('Uhh Please, Give me  Google Drive Url') 
 if (!(text && text.match(/drive\.google/i))) citel.reply('Uhh Please, Give me Valid Google Drive Url')
-   
 let id =(text.match(/\/?id=(.+)/i) || text.match(/\/d\/(.*?)\//))[1]
 if (!id) return citel.reply('ID Not Found');
 try {
@@ -360,6 +359,27 @@ try {
  } catch (error) {  return citel.reply("```File Not Found```" ) }
 	
 })
+//---------------------------------------------------------------------------
+cmd({
+            pattern: "gitclone",
+            desc: "Downloads apks  .",
+            category: "downloader",
+            filename: __filename,
+            use: '<add sticker url.>',
+        },
+        async(Void, citel, text) => {
+	if (!text) return await citel.reply('*Provide Repo Url, Ex:- _.gitclone https://github.com/SuhailTechInfo/Secktor-bot_*') 
+    const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
+    if (!regex.test(text) ) return await citel.reply('*Uhh Please, Provide Valid Repositry Url*');
+    let [_, user, repo] = text.match(regex) || []
+    repo = repo.replace(/.git$/, '')
+    let url = `https://api.github.com/repos/${user}/${repo}/zipball`
+    let filename = (await fetch(url, { method: 'HEAD' })).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
+    //citel.send(`✳️ Wait, sending repository.. \n` + filename.toString() )
+	await Void.sendMessage(citel.chat , {document : { url : url }, fileName:  filename,mimetype: 'application/zip',  })
+
+	})
+
   //---------------------------------------------------------------------------
 cmd({
             pattern: "tts",
