aeb9bf8c1aca63fb5fc6b7862fd5a62530b39c0e
WASI <142110420+Itxxwasi@users.noreply.github.com>
2024-02-24 09:34:07 +0500

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]
 global.style = process.env.STYLE   || '5',  // put '1' to "5" here to check bot styles
 global.flush = process.env.FLUSH   || "true"; // Make it "true" if bot not responed
 global.gdbye = process.env.GOODBYE || "false"; 
-global.wlcm  = process.env.WELCOMEWELCOM"false";  // Make it "false" for disable WELCOME 
+global.wlcm  = process.env.WELCOMEWELCOM || "false";  // Make it "false" for disable WELCOME 
 
 global.warncount = process.env.WARN_COUNT || 3,
 global.disablepm = process.env.DISABLE_PM || "false",
